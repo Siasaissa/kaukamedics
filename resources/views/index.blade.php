8292e957d8f5644b8a71399247d46bcb9d1b04b3
Ahmad Siasa <ahmadsiasa45@gmail.com>
2026-03-03 21:47:21 +0300

    little more

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -95,7 +95,13 @@
                         </ul>
                         <ul class="nav navbar-nav navbar-right">
 
-                            <li class="nav-item"><a href="#" class="cart"><span class="ti-bag badge rounded-pill bg-danger">{{ session('cart') ? count(session('cart')) : 0 }}</span></a></li>
+                            <li class="nav-item">
+    <a href="#" class="cart">
+        <span class="ti-bag badge rounded-pill bg-danger">
+            {{ session('cart') ? count(session('cart')) : 0 }}
+        </span>
+    </a>
+</li>
                             <li class="nav-item">
                                 <button class="search"><span class="lnr lnr-magnifier" id="search"></span></button>
                             </li>
