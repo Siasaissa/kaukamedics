cc908c06c4e8abc2675a0b324f9eae70394a5d5d
Ahmad Siasa <ahmadsiasa45@gmail.com>
2026-03-03 21:48:11 +0300

    again

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -95,13 +95,9 @@
                         </ul>
                         <ul class="nav navbar-nav navbar-right">
 
-                            <li class="nav-item">
-    <a href="#" class="cart">
-        <span class="ti-bag badge rounded-pill bg-danger">
-            {{ session('cart') ? count(session('cart')) : 0 }}
-        </span>
-    </a>
-</li>
+                            <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+    {{ session('cart') ? count(session('cart')) : 0 }}
+</span>
                             <li class="nav-item">
                                 <button class="search"><span class="lnr lnr-magnifier" id="search"></span></button>
                             </li>
