5ae41b7b3fa3d29e19e9e369f54940008302718c
Ahmad Siasa <ahmadsiasa45@gmail.com>
2026-03-05 13:47:53 +0300

    q

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -230,7 +230,7 @@
                             <div class="single-deal">
                                 <div class="overlay"></div>
                                 <img class="img-fluid w-100" src="img/medicalequipment.jpg" alt="Medical Equipment">
-                                <a href="img/category/c1.jpg" class="img-pop-up" target="_blank">
+                                <a href="img/medicalequipment.jpg" class="img-pop-up" target="_blank">
                                     <div class="deal-details">
                                         <h6 class="deal-title">Medical Equipment</h6>
                                     </div>
@@ -240,7 +240,7 @@
                         <div class="col-lg-4 col-md-4">
                             <div class="single-deal">
                                 <div class="overlay"></div>
-                                <img class="img-fluid w-100" src="img/category/c2.jpg" alt="Hospital Consumables">
+                                <img class="img-fluid w-100" src="img/Hospital_consumable.jpg" alt="Hospital Consumables">
                                 <a href="img/Hospital_consumable.jpg" class="img-pop-up" target="_blank">
                                     <div class="deal-details">
                                         <h6 class="deal-title">Hospital Consumables</h6>
