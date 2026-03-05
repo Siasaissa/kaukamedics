a00312bd8185ef99b99b80eadc02d080e1eae1f6
Ahmad Siasa <ahmadsiasa45@gmail.com>
2026-03-05 13:59:49 +0300

    r

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -251,8 +251,8 @@
                         <div class="col-lg-4 col-md-4">
                             <div class="single-deal">
                                 <div class="overlay"></div>
-                                <img class="img-fluid w-100" src="img/category/c3.jpg" alt="Patient Care Products">
-                                <a href="img/category/c3.jpg" class="img-pop-up" target="_blank">
+                                <img class="img-fluid w-100" src="img/patient_care.jpg" alt="Patient Care Products">
+                                <a href="img/patient_care.jpg" class="img-pop-up" target="_blank">
                                     <div class="deal-details">
                                         <h6 class="deal-title">Patient Care Products</h6>
                                     </div>
