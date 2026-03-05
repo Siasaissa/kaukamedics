1191f03ba63764e29511f1acac25a2e18f52abe2
Ahmad Siasa <ahmadsiasa45@gmail.com>
2026-03-05 13:03:29 +0300

    f

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -46,16 +46,16 @@
     <!-- Topbar End -->
 
     <!-- Navbar Start (integrated with header area) -->
-    <header class="header_area sticky-header">
+    <header class="header_area sticky-header" style="margin: 15px">
         <div class="main_menu">
             <nav class="navbar navbar-expand-lg navbar-light main_box" style="padding-top:0 !important; padding-bottom:0 !important;">
                 <div class="container">
                     <!-- Brand and toggle get grouped for better mobile display -->
                     <a class="navbar-brand logo_h d-flex" href="#" style="padding:0 !important; margin:0 !important; line-height:0 !important;">
-    <img src="img/logo.png" 
-         alt="Kauka Company Logo" 
-         style="height:120px !important; padding:0 !important; margin:0 !important; display:block;">
-</a>
+                        <img src="img/logo.png" 
+                            alt="Kauka Company Logo" 
+                            style="height:120px !important; padding:0 !important; margin:0 !important; display:block;">
+                    </a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="icon-bar"></span>
