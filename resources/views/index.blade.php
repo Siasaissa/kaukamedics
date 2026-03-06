de4c5c3fe59eb9fb04731475faf6e9cd06b6fedc
Ahmad Siasa <ahmadsiasa45@gmail.com>
2026-03-06 12:21:11 +0300

    z

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -468,27 +468,6 @@
     </section>
     <!-- End exclusive deal Area -->
 
-    <!-- Start Video / Company Overview (from provided content) -->
-    <div class="container-fluid bg-primary mb-5 wow fadeIn" data-wow-delay="0.1s">
-        <div class="container">
-            <div class="row g-0">
-                <div class="col-lg-11">
-                    <div class="h-100 py-5 d-flex align-items-center">
-                        <button type="button" class="btn-play" data-bs-toggle="modal"
-                            data-src="img/video1.mp4" data-bs-target="#videoModal">
-                            <span></span>
-                        </button>
-                        <h3 class="ms-5 mb-0 text-white">Delivering trusted medical supplies that empower healthcare professionals to save lives every day.</h3>
-                    </div>
-                </div>
-                <div class="d-none d-lg-block col-lg-1">
-                    <div class="h-100 w-100 bg-secondary d-flex align-items-center justify-content-center">
-                        <span class="text-white" style="transform: rotate(-90deg);">Scroll Down</span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
 
     <!-- Video Modal Start -->
     <div class="modal fade" id="videoModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
