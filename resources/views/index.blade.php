50c90ce03fc78d5e0b15e5e4a229df98e1345011
Ahmad Siasa <ahmadsiasa45@gmail.com>
2026-03-06 19:54:46 +0300

    i

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -699,10 +699,7 @@
             </div>
         </div>
     </footer>
-
-    <!-- Back to Top -->
-    <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
-
+    
     <!-- Scripts -->
     <script src="js/vendor/jquery-2.2.4.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
