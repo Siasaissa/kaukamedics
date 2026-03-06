f260d13ded0757c96e17d2ac521a01be5b23154a
Ahmad Siasa <ahmadsiasa45@gmail.com>
2026-03-06 21:00:10 +0300

    little more

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -832,7 +832,7 @@
                             <div class="col-8">
                                 <div class="btn-group w-100" role="group">
                                     <a href="{{ route('add.to.cart', $product->id) }}" class="btn btn-primary">
-                                        <i class="ti-bag mr-1"></i> Inquire
+                                        <i class="ti-bag mr-1"></i>
                                     </a>
                                 </div>
                             </div>
