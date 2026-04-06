eb4820d54a577ead3f66f094a0938f2a90122b31
Ahmad Siasa <ahmadsiasa45@gmail.com>
2026-04-06 12:26:09 +0300

    Refactor payment section: Remove unused payment options and terms acceptance checkbox for a cleaner checkout experience.

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -145,32 +145,7 @@
                                 <li><a href="#">Shipping <span>Flat rate: TZS 2,000</span></a></li>
                                 <li><a href="#">Total <span>TZS {{ number_format($total + 2000, 2) }}</span></a></li>
                             </ul>
-                            <div class="payment_item">
-                                <div class="radion_btn">
-                                    <input type="radio" id="f-option5" name="payment_method" value="check">
-                                    <label for="f-option5">Check payments</label>
-                                    <div class="check"></div>
-                                </div>
-                                <p>Please send a check to Store Name, Store Street, Store Town, Store State / County,
-                                    Store Postcode.</p>
-                            </div>
-                            <div class="payment_item active">
-                                <div class="radion_btn">
-                                    <input type="radio" id="f-option6" name="payment_method" value="paypal" checked>
-                                    <label for="f-option6">Paypal </label>
-                                    <img src="img/product/card.jpg" alt="">
-                                    <div class="check"></div>
-                                </div>
-                                <p>Pay via PayPal; you can pay with your credit card if you don’t have a PayPal
-                                    account.</p>
-                            </div>
-                            <div class="creat_account">
-                                <input type="checkbox" id="f-option4" name="accept_terms">
-                                <label for="f-option4">I’ve read and accept the </label>
-                                <a href="#">terms & conditions*</a>
-                            </div>
-                            <!-- Make this button submit the top form via JS so UI remains same -->
-                            <button id="checkoutSubmit" class="primary-btn">Proceed to Paypal</button>
+                            
                         </div>
                     </div>
                 </div>
