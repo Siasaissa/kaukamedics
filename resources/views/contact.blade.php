06e082970aec6545a189392f473610b88d328657
Ahmad Siasa <ahmadsiasa45@gmail.com>
2026-04-07 12:27:25 +0300

    i

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -111,8 +111,8 @@
 	<!--================Contact Area =================-->
 	<section class="contact_area section_gap_bottom">
 		<div class="container">
-			<div id="mapBox" class="mapBox" data-lat="40.701083" data-lon="-74.1522848" data-zoom="13" data-info="PO Box CT16122 Collins Street West, Victoria 8007, Australia."
-			 data-mlat="40.701083" data-mlon="-74.1522848">
+			<div id="mapBox" class="mapBox" data-lat="-6.810787" data-lon="39.259628" data-zoom="15" data-info="Magomeni Kanisani, Fundikila 10 Street, Dar es Salaam, Tanzania."
+			 data-mlat="-6.810787" data-mlon="39.259628">
 			</div>
 			<div class="row">
 				<div class="col-lg-3">
