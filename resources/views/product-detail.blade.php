9f4ef59a6ac290f467ac542a6cf6478598b759b2
Ahmad Siasa <ahmadsiasa45@gmail.com>
2026-04-07 10:14:28 +0300

    a

--- resources/views/product-detail.blade.php
+++ resources/views/product-detail.blade.php
@@ -2,10 +2,15 @@
 <html lang="zxx" class="no-js">
 
 <head>
+	@php
+		$cssVersion = file_exists(public_path('css/main.css'))
+			? filemtime(public_path('css/main.css'))
+			: time();
+	@endphp
 	<!-- Mobile Specific Meta -->
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<!-- Favicon-->
-	<link rel="shortcut icon" href="img/fav.png">
+	<link rel="shortcut icon" href="{{ asset('img/fav.png') }}?v={{ $cssVersion }}">
 	<!-- Author Meta -->
 	<meta name="author" content="CodePixar">
 	<!-- Meta Description -->
@@ -21,14 +26,14 @@
 	<!--
             CSS
             ============================================= -->
-	<link rel="stylesheet" href="{{asset('css/linearicons.css')}}">
-	<link rel="stylesheet" href="{{asset('css/owl.carousel.css')}}">
-	<link rel="stylesheet" href="{{asset('css/font-awesome.min.css')}}">
-	<link rel="stylesheet" href="{{asset('css/themify-icons.css')}}">
-	<link rel="stylesheet" href="{{asset('css/nice-select.css')}}">
-	<link rel="stylesheet" href="{{asset('css/nouislider.min.css')}}">
-	<link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
-	<link rel="stylesheet" href="{{asset('css/main.css')}}">
+	<link rel="stylesheet" href="{{ asset('css/linearicons.css') }}?v={{ $cssVersion }}">
+	<link rel="stylesheet" href="{{ asset('css/owl.carousel.css') }}?v={{ $cssVersion }}">
+	<link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}?v={{ $cssVersion }}">
+	<link rel="stylesheet" href="{{ asset('css/themify-icons.css') }}?v={{ $cssVersion }}">
+	<link rel="stylesheet" href="{{ asset('css/nice-select.css') }}?v={{ $cssVersion }}">
+	<link rel="stylesheet" href="{{ asset('css/nouislider.min.css') }}?v={{ $cssVersion }}">
+	<link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}?v={{ $cssVersion }}">
+	<link rel="stylesheet" href="{{ asset('css/main.css') }}?v={{ $cssVersion }}">
 </head>
 
 <body>
