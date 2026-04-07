24252721bb2edeac31ee0d4cedd1876f1d4c0828
Ahmad Siasa <ahmadsiasa45@gmail.com>
2026-04-07 10:19:44 +0300

    m

--- resources/views/product-detail.blade.php
+++ resources/views/product-detail.blade.php
@@ -2,15 +2,10 @@
 <html lang="zxx" class="no-js">
 
 <head>
-	@php
-		$cssVersion = file_exists(public_path('css/main.css'))
-			? filemtime(public_path('css/main.css'))
-			: time();
-	@endphp
 	<!-- Mobile Specific Meta -->
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<!-- Favicon-->
-	<link rel="shortcut icon" href="{{ asset('img/fav.png') }}?v={{ $cssVersion }}">
+	<link rel="shortcut icon" href="img/fav.png">
 	<!-- Author Meta -->
 	<meta name="author" content="CodePixar">
 	<!-- Meta Description -->
@@ -26,14 +21,14 @@
 	<!--
             CSS
             ============================================= -->
-	<link rel="stylesheet" href="{{ asset('css/linearicons.css') }}?v={{ $cssVersion }}">
-	<link rel="stylesheet" href="{{ asset('css/owl.carousel.css') }}?v={{ $cssVersion }}">
-	<link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}?v={{ $cssVersion }}">
-	<link rel="stylesheet" href="{{ asset('css/themify-icons.css') }}?v={{ $cssVersion }}">
-	<link rel="stylesheet" href="{{ asset('css/nice-select.css') }}?v={{ $cssVersion }}">
-	<link rel="stylesheet" href="{{ asset('css/nouislider.min.css') }}?v={{ $cssVersion }}">
-	<link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}?v={{ $cssVersion }}">
-	<link rel="stylesheet" href="{{ asset('css/main.css') }}?v={{ $cssVersion }}">
+	<link rel="stylesheet" href="css/linearicons.css">
+	<link rel="stylesheet" href="css/owl.carousel.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/themify-icons.css">
+	<link rel="stylesheet" href="css/nice-select.css">
+	<link rel="stylesheet" href="css/nouislider.min.css">
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/main.css">
 </head>
 
 <body>
