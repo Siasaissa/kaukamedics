2a26235cd34a97ddcd2492459db65b7b79ebbd02
Ahmad Siasa <ahmadsiasa45@gmail.com>
2026-04-08 23:25:10 +0300

    b

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -8,12 +8,17 @@
                             alt="Kauka Company Logo" 
                             style="height:100px !important; padding:0 !important; margin:0 !important; display:block;">
                     </a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                     aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
+                    <div class="d-flex align-items-center d-lg-none">
+                        <a href="{{ route('cart') }}" class="cart mr-3" aria-label="Open cart">
+                            <span class="ti-bag text-primary">{{ session('cart') ? count(session('cart')) : 0 }}</span>
+                        </a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                    </div>
                     <!-- Collect the nav links, forms, and other content for toggling -->
                     <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
                         <ul class="nav navbar-nav menu_nav ml-auto">
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -8,12 +8,17 @@
             <div class="container">
                 <!-- Brand and toggle get grouped for better mobile display -->
                 <a class="navbar-brand logo_h" href="{{ url('/') }}"><img src="{{ asset('img/fav.png') }}" alt=""></a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
+                <div class="d-flex align-items-center d-lg-none">
+                    <a href="{{ url('/cart') }}" class="cart mr-3" aria-label="Open cart">
+                        <span class="ti-bag text-primary">{{ session('cart') ? count(session('cart')) : 0 }}</span>
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
                 <!-- Collect the nav links, forms, and other content for toggling -->
                 <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
                     <ul class="nav navbar-nav menu_nav ml-auto">
